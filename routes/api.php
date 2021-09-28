4382774edd04260db300396afa36edb1bfe77f49
khansaimani <khansaimani1919@gmail.com>
2021-09-28 20:32:56 +0700

    challenge-author

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,4 +19,10 @@ use App\Http\Controllers\BookController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('books', BookController::class)->except('create', 'edit');
+
+Route::get('books', [BookController::class, 'index']);
+Route::post('books', [BookController::class, 'store']);
+Route::get('books/{id}', [BookController::class, 'show']);
+Route::put('books/{id}', [BookController::class, 'update']);
+Route::delete('books/{id}', [BookController::class, 'destroy']);
+Route::resource('authors', AuthorController::class);
